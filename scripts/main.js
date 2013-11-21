84b32a85218c3e794ae96183475a0f1d6bd0ea3a
VieuxChameau <VieuxChameau@users.noreply.github.com>
2013-11-21 23:00:18 +0100

    Basic js + cool links

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+console.log("Train #" + 1 + " is running.");
+
+var eruption1 = 112;
+var eruption2 = 148;
+var eruption3 = 160;
+var numEruptions = 3;
+var avg = (eruption1 + eruption2 + eruption3) / numEruptions;
+console.log("\"Old Faithful\" at Yellowstone National Park");
+console.log("Eruption 1: " + eruption1);
+console.log("Eruption 2: " + eruption2);
+console.log("Eruption 3: " + eruption3);
+console.log("Average eruption height today: " + avg);
---
+++ scripts/string.js
@@ -0,0 +1,10 @@
+var escapeString = "Quoth the raven:\n\t\"Nevermore!\"";
+
+var concatString = "eight " + 5+ " six two " + 3 + " " + 7;
+
+var length = concatString.length;
+
+var at = concatString.charAt(5);
+
+// http://www.impressivewebs.com/javascript-string-methods-reference/
+// http://www.javascriptkit.com/jsref/escapesequence.shtml
