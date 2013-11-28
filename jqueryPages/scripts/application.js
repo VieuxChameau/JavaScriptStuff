6a6cdcc4b47b86998ee7bc8efea946e58ecbd9f2
VieuxChameau <VieuxChameau@users.noreply.github.com>
2013-11-28 23:09:19 +0100

    jquery handling event

--- jqueryPages/scripts/application.js
+++ jqueryPages/scripts/application.js
@@ -87,4 +87,21 @@ jQuery(document).ready(function() {
         $(".tour").filter(".featured").addClass("highlight");
     });
 
+    function showPhotos() {
+        $(this).find("span").slideToggle();
+    }
+    $("#tour").on("click", "button", function() {
+        $(".photos").slideToggle();
+    });
+    $(".photos").on("mouseenter", "li", showPhotos).on("mouseleave", "li", showPhotos);
+
+
+    $("#nights").on("keyup", function() {
+        var nights = +$(this).val();
+        var dailyPrice = +$(this).closest(".tour").data("daily-price");
+        $("#total").text(nights * dailyPrice);
+        $("#nights-count").text($(this).val());
+    }).on("focus", function() {
+        $(this).val(7);
+    });
 });
